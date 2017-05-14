d19f6990289158393165dad57f02f2b55b5b5c27
anxcon <okthissux@hotmail.com>
2017-05-14 09:55:47 -0700

    initial

---
+++ AnimatedDocking/ModuleAnimatedDocking.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using KSP;
+
+namespace AnimatedDocking
+{
+    public class ModuleAnimatedDocking : PartModule
+    {
+        private ModuleAnimateGeneric module;
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+            this.module = base.part.FindModuleImplementing<ModuleAnimateGeneric>();
+            if (this.module == null) return;
+            GameEvents.onPartCouple.Add(this.onPartCouple);
+            GameEvents.onPartUndock.Add(this.onPartUndock);
+        }
+        private void onPartCouple(GameEvents.FromToAction<Part, Part> action)
+        {
+            if (action.from == base.part)
+            {
+                module.Toggle();
+            }
+        }
+        private void onPartUndock(Part part)
+        {
+            if (part = base.part)
+            {
+                module.Toggle();
+            }
+        }
+        private void OnDestroy()
+        {
+            GameEvents.onPartCouple.Add(this.onPartCouple);
+            GameEvents.onPartUndock.Remove(this.onPartUndock);
+        }
+    }
+}
